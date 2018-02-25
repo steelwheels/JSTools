d6999027c392ab3c6afff2d426e779dc546aa986
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2018-02-25 21:30:54 +0900

    sample-code: Add "LifeGame" scripts

--- Resource/Game/LifeGame/Main.js
+++ Resource/Game/LifeGame/Main.js
@@ -2,7 +2,27 @@
 
 function main()
 {
-  console.log("Hello, world !!\n") ;
+  /* setup screen mode */
+  console.setScreenMode(true) ;
+  console.doBuffering = false ;
+
+  const fields = [null, null]
+  fields[0] = new Field(console.screenWidth, console.screenHeight) ;
+  fields[1] = new Field(console.screenWidth, console.screenHeight) ;
+  fields[0].randomize() ;
+  fields[0].draw() ;
+
+  let curidx = 0 ;
+
+  /* Wait user press some key */
+  while(console.getKey() != "q"){
+    let nextidx = (curidx == 0) ? 1 : 0 ;
+    fields[nextidx].update(fields[curidx]) ;
+    fields[nextidx].draw() ;
+    curidx = nextidx ;
+  }
+  console.setScreenMode(false) ;
+  console.log("Finish program\n") ;
 }
 
 main() ;
