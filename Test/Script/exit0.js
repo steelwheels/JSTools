eb32d94268a735b8ab72a4e96d665aecb0e6eef3
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2018-11-09 01:47:15 +0900

    test: Update test for exit() method

--- Test/Script/exit0.js
+++ Test/Script/exit0.js
@@ -2,7 +2,7 @@
 function main(args)
 {
 	console.log("Before exit: " + args + "\n") ;
-	Process.exit(1) ;
+	exit(1) ;
 	console.log("After exit\n") ;
 }
 
--- Test/unit_test.mk
+++ Test/unit_test.mk
@@ -54,11 +54,11 @@ exit0: dummy
 	@echo "*** test: exit0 ***"
 	(if $(jsrun) --use-main $(script_dir)/exit0.js -- a b c | \
 					tee $(build_dir)/exit0.txt ; then \
-		exit 0 ; \
+		echo "expected error" ; \
 	 else \
 		exit 1 ; \
 	 fi)
-	diff $(build_dir)/exit0.txt $(expected_dir)/exit0.txt
+	diff $(build_dir)/exit0.txt $(expected_dir)/exit0.txt 
 
 exit1: dummy
 	@echo "*** test: exit1 ***"
