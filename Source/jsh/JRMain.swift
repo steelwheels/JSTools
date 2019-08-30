43d7bd906d9a101ba236a48186166d166640d753
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-08-31 00:48:36 +0900

    jsh: Support script execution on the thread

--- Source/jsh/JRMain.swift
+++ Source/jsh/JRMain.swift
@@ -17,7 +17,7 @@ public func main(arguments args: Array<String>) -> Int32
 	/* Parse command line arguments */
 	let console = CNFileConsole()
 	let parser  = JRCommandLineParser(console: console)
-	guard let (options, params) = parser.parseArguments(arguments: Array(args.dropFirst())) else { // drop application name
+	guard let (config, params) = parser.parseArguments(arguments: Array(args.dropFirst())) else { // drop application name
 		return 1
 	}
 
@@ -26,78 +26,41 @@ public func main(arguments args: Array<String>) -> Int32
 		console.error(string: "Failed to allocate VM\n")
 		return 1
 	}
-	let context  = KEContext(virtualMachine: vm)
-	let config   = KEConfig(kind: .Terminal, doStrict: true, doVerbose: options.doVerbose)
+	let compconf = KHConfig(kind: .Terminal, hasMainFunction: config.doUseMain, doStrict:true, doVerbose: config.doVerbose)
 	let env      = CNShellEnvironment()
-	let compiler = KHShellCompiler()
-	guard compiler.compile(context: context, environment: env, console: console, config: config) else {
-		console.error(string: "Internal compile error\n")
-		return 1
-	}
 
-	if params.count == 0 || options.isInteractiveMode {
+	if params.count == 0 || config.isInteractiveMode {
 		/* Execute shell */
-		return execute(context: context, scriptFiles: params, environment: env, console: console, config: config)
+		return execute(virtualMachine: vm, scriptFiles: config.scriptFiles, environment: env, console: console, config: compconf)
 	} else {
 		/* Execute script */
-		return execute(context: context, scriptFiles: params, environment: env, console: console, config: config)
+		return execute(virtualMachine: vm, scriptFiles: config.scriptFiles, environment: env, console: console, config: compconf)
 	}
 }
 
-private func execute(context ctxt: KEContext, scriptFiles files: Array<String>, environment env: CNShellEnvironment, console cons: CNConsole, config conf: KEConfig) -> Int32
+private func execute(virtualMachine vm: JSVirtualMachine, scriptFiles files: Array<String>, environment env: CNShellEnvironment, console cons: CNConsole, config conf: KHConfig) -> Int32
 {
 	/* Allocate shell interface */
 	let intf = CNShellInterface()
-	intf.input.setWriter(handler: {
-		() -> String? in
-		let data = FileHandle.standardInput.availableData
-		let str  = String(data: data, encoding: .utf8)
-		return str
-	})
-	intf.output.setReader(handler: {
-		(_ str: String) -> Void in
-		if let data = str.data(using: .utf8) {
-			FileHandle.standardOutput.write(data)
-		} else {
-			NSLog("Failed to geneeate output")
-		}
-	})
-	intf.error.setReader(handler: {
-		(_ str: String) -> Void in
-		if let data = str.data(using: .utf8) {
-			FileHandle.standardError.write(data)
-		} else {
-			NSLog("Failed to geneeate output")
-		}
-	})
+	intf.connectWithStandardInput()
+	intf.connectWithStandardOutput()
+	intf.connectWithStandardError()
 
-	let shell  = KHShell(context: ctxt, shellInterface: intf, environment: env, console: cons, config: conf)
-	shell.start()
+	/* Get URLs for script */
+	var urls: Array<URL> = []
+	for file in files {
+		urls.append(URL(fileURLWithPath: file))
+	}
+
+	let thread  = KHScriptThread(virtualMachine: vm, shellInterface: intf, environment: env, console: cons, config: conf)
+	thread.start(userScripts: urls, arguments: [])
 	sleep(10)
 
 	/* Wait until finished */
-	while shell.isExecuting {
+	while thread.isExecuting {
 
 	}
 
 	return 0
 }
 
-/*
-
-
-
-	/* start shell */
-
-
-
-
-	/*
-	let intf   = CNShellInterface(input: stdin, output: stdout, error: stderr)
-
-	let console = CNFileConsole()
-	console.print(string: "Hello from jsh !!\n")
-*/
-	return 0
-}
-*/
--- Test/Script/shell1.js
+++ Test/Script/shell1.js
@@ -1,8 +1,7 @@
-let lscmd = Shell.searchCommand("ls") ;
-console.log("ls command = " + lscmd + "\n") ;
+//let lscmd = Shell.searchCommand("ls") ;
+//console.log("ls command = " + lscmd + "\n") ;
 
 console.log("exec echo command = ") ;
-let process = Shell.execute("echo \"Hello, world !!\"", null, null, null) ;
+let process = system("echo \"Hello, world !!\"", null) ;
 process.waitUntilExit() ;
-
 
