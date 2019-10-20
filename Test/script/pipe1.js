df0c0f92c3853fefa35882bf20d41560466f1fcb
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-10-20 21:29:43 +0900

    test: Update unit test: pipe1.js

--- Test/script/pipe1.js
+++ Test/script/pipe1.js
@@ -13,17 +13,20 @@ if(process == null){
 	exit(1) ;
 }
 
-console.log("Wait until exit\n") ;
+console.log("Wait until exit ... begin\n") ;
 process.waitUntilExit() ;
 pipe.writing.close() ;
+console.log("Wait until exit ... done\n") ;
 
 /* receive output */
+console.log("Receive output... begin\n") ;
 let reader = pipe.reading ;
 let c = reader.getc() ;
 while(c != null){
 	console.log(`[receive] ${c}\n`) ;
 	c = reader.getc() ;
 }
+console.log("Receive output... done\n") ;
 
 console.log("[bye]\n");
 
