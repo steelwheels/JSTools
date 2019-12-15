27c1929724e1aca308b367b76b40224576455813
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-12-15 22:35:18 +0900

    test: Add sample script for jsh: curses0.js

--- Sample/curses0.js
+++
@@ -1,8 +0,0 @@
-console.log("setup curses start") ;
-
-Curses.mode(true) ;
-Curses.put("Hello, world\n") ;
-Curses.mode(false) ;
-
-console.log("Good bye, world") ;
-
---
+++ Test/script/curses0.js
@@ -0,0 +1,6 @@
+curses.initScreen() ;
+let width  = curses.width ;
+let height = curses.height ;
+
+console.log("width = " + width + ", height = " + height) ;
+
