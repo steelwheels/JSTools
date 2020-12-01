6a7ec3bbf599e0eb871824742a715ba10ec8ad15
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2020-12-01 22:51:02 +0900

    test: Update unit test for amb

--- Test/amb_test.mk
+++ Test/amb_test.mk
@@ -5,14 +5,18 @@
 bin_dir	= $(HOME)/tools/bin
 amb_cmd = $(bin_dir)/amb
 
-all: hello0
+all: hello0 synerr0
 
 clean:
 	rm -f *.amb.log
 
 hello0: dummy
-	$(amb_cmd) --log detail ./script/hello0.amb > hello0.amb.log
+	$(amb_cmd) --log detail ./script/hello0.amb >& hello0.amb.log
 	diff hello0.amb.log expected/hello0.amb.log.OK
+
+synerr0: dummy
+	$(amb_cmd) --log detail ./script/synerr0.amb >& synerr0.amb.log || \
+	diff synerr0.amb.log expected/synerr0.amb.log.OK
 
 dummy:
 
