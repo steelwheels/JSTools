ca2ad96e9e522c92133ceb985af4cacbb05d245f
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2021-08-29 16:27:58 +0900

    script: Update sample script

--- Test/script/json0.js
+++ Test/script/json0.js
@@ -1,13 +1,13 @@
 
 function json0(){
- 	var data = JSON.read("../Sample/data0.json") ;
+ 	var data = JSON.read(stdin) ;
 	console.log("a = " + data.a) ;
 	console.log(data) ;
 
 	data.a += 1 ;
 	data.c =  0xff ;
 
-	var res = JSON.write("data0-out.json", data) ;
+	var res = JSON.write(stdout, data) ;
 	if(res != 0){
 		console.error("Failed to write JSON: " + res + "\n") ;
 	}
